2b10b919638de489c2b99ab4d2dc18629047a800
rooslindeboom <rooslindeboom@outlook.com>
2022-09-18 19:33:12 +0200

    added socket.io

--- 1._friend_pane/app.js
+++ 1._friend_pane/app.js
@@ -1,13 +1,25 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 const http = require('http');
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+      });
 });
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// });
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
+
